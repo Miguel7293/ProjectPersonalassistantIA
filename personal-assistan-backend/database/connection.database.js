52940a68e945e6bfc048a559d94a41ff7dc6b323
MiguelwLD <114840612+Miguel7293@users.noreply.github.com>
2024-11-06 00:26:58 -0500

    miniModificaciones
    para le funcionamiento

--- personal-assistan-backend/database/connection.database.js
+++ personal-assistan-backend/database/connection.database.js
@@ -3,6 +3,7 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
+
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -19,3 +20,5 @@ export const connectDB = async () => {
     console.error('Error connecting to PostgreSQL database:', error);
   }
 };
+
+connectDB(); // Asegúrate de llamar la función para iniciar la conexión
